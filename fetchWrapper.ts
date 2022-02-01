7b28f5df4182a993af78ea6b6653fb696b46c4d1
kirtan <kirtanmagan@gmail.com>
2022-01-31 21:40:04 -0800

    Added change user profile picture. For now, this only uploads the file to the server and not in the database.

--- fetchWrapper.ts
+++ fetchWrapper.ts
@@ -1,22 +1,39 @@
 class Server {
-  static dev = "http://localhost:7090";
-  static prod = "https://bloggy.raakeshpatel.com/api";
+  static dev = 'http://localhost:7090'
+  static prod = 'https://bloggy.raakeshpatel.com/api'
 
-  static get url() {
-    return process.env.NODE_ENV == "development" ? this.dev : this.prod;
+  static get url () {
+    return process.env.NODE_ENV == 'development' ? this.dev : this.prod
   }
 }
 
 const Post = async (endpoint: string, body: string) => {
   const post_data = await fetch(`${Server.url}/${endpoint}`, {
-    method: "POST",
-    mode: "cors",
+    method: 'POST',
+    mode: 'cors',
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
     body,
-  });
-  return await post_data.json();
-};
+    redirect: 'follow'
+  })
+  return await post_data.json()
+}
 
-export { Server, Post };
+const FormPost = async (
+  endpoint: string,
+  file_type_name: string,
+  file: any
+) => {
+  const new_form = new FormData()
+  new_form.append(file_type_name, file) // Append the type of file and the file itself
+  const post_data = await fetch(`${Server.url}/${endpoint}`, {
+    method: 'POST',
+    mode: 'cors',
+    body: new_form,
+    redirect: 'follow'
+  })
+  return await post_data.json()
+}
+
+export { Server, Post, FormPost }
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import React, { useState, useEffect } from 'react'
 import Router from 'next/router'
-import { Post } from '../fetchWrapper'
+import { Post, FormPost } from '../fetchWrapper'
 import UserVerifyChecks from '../util/UserVerifyChecks'
 
 export default function Settings () {
@@ -29,14 +29,16 @@ export default function Settings () {
     }
   }
 
+  // Change the user's name
   const changeUsername = async () => {
     if (!username) {
       alert('Please enter a new username.')
       return
     }
 
-    const promptInfo = doPromptUsernameChange()
+    const promptInfo = doPromptUsernameChange() // Make the prompt
 
+    // If the user didn't input any data, run this function over and over until they do
     if (promptInfo[0] === null || promptInfo[1] === null) {
       alert('Please enter an email and password.')
       changeUsername()
@@ -61,6 +63,35 @@ export default function Settings () {
     alert(
       `Successfully changed username to '${username}'. (You must now wait 1 week to change it again)`
     )
+  }
+
+  const clickPfpInput = () => {
+    const realPfpUpload = document.querySelector<HTMLElement>('#upload_new_pfp')
+    if (realPfpUpload !== null) realPfpUpload?.click()
+  }
+
+  // Make a request to change the users profile picture
+  const uploadNewPfp = async (event: any) => {
+    const file = event?.target?.files[0]
+
+    const fileName = file?.name
+    const fileExtension = fileName
+      ?.split('.')
+      ?.pop()
+      ?.toLowerCase()
+    if (
+      fileExtension !== 'png' &&
+      fileExtension !== 'jpg' &&
+      fileExtension !== 'jpeg'
+    ) {
+      alert('Please select a valid image file (png, jpg, and jpeg only).')
+      return
+    }
+    const post_form = await FormPost('upload-pfp', 'pfp', file)
+    if (!post_form.success) {
+      alert(post_form.message)
+      return
+    }
   }
 
   return (
@@ -96,7 +127,19 @@ export default function Settings () {
             width={100}
             height={100}
           />
-          <button className='btn' id='submit-photo-change'>
+          <input
+            style={{ display: 'none' }}
+            type='file'
+            name='pfp'
+            id='upload_new_pfp'
+            accept='.png,.jpeg,.jpg'
+            onChange={uploadNewPfp}
+          ></input>
+          <button
+            onClick={clickPfpInput}
+            className='btn hover-button-pink'
+            id='submit-photo-change'
+          >
             Change Photo
           </button>
         </div>
@@ -112,7 +155,7 @@ export default function Settings () {
           <button
             onClick={changeUsername}
             id='change-username-btn'
-            className='btn'
+            className='btn hover-button-pink'
           >
             Change
           </button>
